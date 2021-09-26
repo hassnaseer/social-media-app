a5b909930fe0230e5f8f3a67151ea9e4726382f3
malikhassam <malik.hassam531@gmail.com>
2021-09-26 11:21:37 +0500

    amity kit

--- src/accClient.js
+++ src/accClient.js
@@ -2,7 +2,7 @@ import ASCClient, { ConnectionStatus, ApiEndpoint } from "@amityco/js-sdk";
 
 // Amity sample-app apiKey
 const apiKey = "b3bee90c39d9a5644831d84e5a0d1688d100ddebef3c6e78";
-export const client = ASCClient.create({ apiKey, apiEndpoint: ApiEndpoint.SG });
+export const client = ASCClient.create({ apiKey, apiEndpoint: ApiEndpoint.EU });
 
 export let currentUserId = null;
 
